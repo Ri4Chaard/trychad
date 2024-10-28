a0eec65769b1913f3d0b595f92cd2fc2c87c4f36
Ri4Chaard <yarik271202@gmail.com>
2024-10-28 18:11:00 +0200

    Check animation

--- components/shared/success-circle.tsx
+++ components/shared/success-circle.tsx
@@ -1,25 +1,34 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 import { Check } from "lucide-react";
+import { motion } from "framer-motion";
 
 interface Props {
     checkSize?: number;
     className?: string;
 }
 
-export const SuccessCircle: React.FC<Props> = ({ checkSize, className }) => {
+export const SuccessCircle: React.FC<Props> = ({
+    checkSize = 40,
+    className,
+}) => {
     return (
-        <div
+        <motion.div
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{ duration: 0.5, ease: "easeOut" }}
             className={cn(
                 "flex items-center justify-center w-20 h-20 rounded-full bg-[#65BD47] text-white",
                 className
             )}
         >
-            <Check
-                width={checkSize || 40}
-                height={checkSize || 40}
-                strokeWidth={3}
-            />
-        </div>
+            <motion.div
+                initial={{ rotate: -45, scale: 0 }}
+                animate={{ rotate: 0, scale: 1 }}
+                transition={{ delay: 0.3, duration: 0.3, ease: "easeOut" }}
+            >
+                <Check width={checkSize} height={checkSize} strokeWidth={3} />
+            </motion.div>
+        </motion.div>
     );
 };
